3ff7340c90c9881f5930caf7b9a7b47638c9639d
Marco Moauro <marco.moauro@translated.net>
2025-05-08 23:08:40 +0200

    working server and tool

--- src/tools/create_draft_post.js
+++ src/tools/create_draft_post.js
@@ -1,10 +1,18 @@
-import { z } from "zod";
+import {z} from "zod";
 
 
-export const createDraftPostSchema = {
+export const createDraftPostSchema = z.object({
+  text: z
+    .string()
+    .describe(
+      "The text of the post to be created."
+    ),
+});
 
-}
+export const createDraftPostHandler = async (args) => {
+  const validatedArgs = createDraftPostSchema.parse(args);
 
-export const createDraftPostHandler = async () => {
+  const { text } = validatedArgs;
 
+  return text
 }
